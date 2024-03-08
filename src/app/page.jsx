c0f43ab94b38059c5169fd11ef5a99a671da1302
umarkhan110 <umardraz277@gmail.com>
2024-03-08 22:51:27 +0500

    updated design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,42 +9,34 @@ export default function Home() {
   const translateText = async () => {
     setLoader(true);
     const response = await translate(input);
-    // console.log(response.translateText)
-    // console.log(response.translatedText.content[0].text)
     setTransltedText(response?.translatedText?.content[0]?.text);
     setLoader(false);
   };
   return (
-    <div style={{margin:"10px"}}>
-      <h4>Translate Classical Armenian language to Modren Armenian language</h4>
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          margin: "50px 20px",
-        }}
-      >
-        <div style={{marginRight:"50px"}}>
-        <h6>Enter Classical Armenian language</h6>
-        <br />
+    <div style={{ margin: "10px" }}>
+      <h4 className="text-center">
+        Translate Classical Armenian language to Modern Armenian language
+      </h4>
+
+      <div className="row justify-content-center mt-5" style={{ marginLeft: '100px', marginRight: '100px' }}>
+        <div className="col-md-6 mb-4">
+          <h6>Enter Classical Armenian language</h6>
           <textarea
-            type="text"
+            className="form-control mb-3"
+            rows="5"
             value={input}
             onChange={(e) => setInput(e.target.value)}
             placeholder="Enter Classical Armenian language"
-            style={{  height: "300px", marginBottom: "10px", padding:"20px" }}
-          />
-          <br />
-          <button onClick={translateText} style={{ padding:"10px 15px", borderRadius:"8px"}}>Translate</button>
-          <br />
-          <br />
+          ></textarea>
+          <button className="btn btn-primary" onClick={translateText}>
+            Translate
+          </button>
         </div>
-        <div style={{height:"300px", width:"500px", marginLeft:"50px"}}>
-          <h6>Modren Armenian language</h6>
-          <br />
-          <div style={{height:"100%", border:"1px solid black", padding:"20px"}}>
-          {loader ? <p>Loading...</p> : transltedText }
+
+        <div className="col-md-6">
+          <h6>Modern Armenian language</h6>
+          <div className="border p-3" style={{ minHeight: "200px" }}>
+            {loader ? <p>Loading...</p> : transltedText}
           </div>
         </div>
       </div>

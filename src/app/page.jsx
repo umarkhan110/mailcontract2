a0ba93f23da2e5760309d883a7a324415c4e2c76
umarkhan110 <umardraz277@gmail.com>
2024-03-08 18:47:33 +0500

    fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,68 @@
 "use client";
 import Anthropic from "@anthropic-ai/sdk";
+import { useState } from "react";
 export default function Home() {
+  const [input, setInput] = useState("")
+  const [transltedText, setTransltedText] = useState("")
+
   const anthropic = new Anthropic({
     apiKey: process.env.NEXT_PUBLIC_CLAUDE,
   });
-
-  const main = async () => {
-    async function main() {
-      const message = await anthropic.messages.create({
-        max_tokens: 1024,
-        messages: [{ role: 'user', content: 'Hello, Claude' }],
-        model: 'claude-3-opus-20240229',
-      });
-    
-      console.log(message.content);
-    }
-    
-    main();
-  };
+const translate = async () =>{
+  const msg = await anthropic.messages.create({
+    model: "claude-3-opus-20240229",
+    max_tokens: 1000,
+    temperature: 0,
+    system: "Translate classical Armenian language to modern Armenian language\nUser will write classical Armenian language and we will give him translated text(modern Armenian language) in response",
+    messages: [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": "Իգական"
+          }
+        ]
+      }
+    ]
+  });
+  setTransltedText(msg.content)
+}
   return (
     <>
-    <button onClick={main}>ABC</button>
+    <input type="textarea" value={input} onChange={(e)=>setInput(e.target.value)} placeholder="Classical Armenian language"/><br />
+    <button onClick={translate}>Translate</button><br /><br />
+    <div>
+      Modren Armenian language
+      {transltedText}</div>
     </>
   );
 }
+
+
+
+
+// import Anthropic from "@anthropic-ai/sdk";
+// // const client = new Anthropic();
+// const client = new Anthropic({
+//     apiKey: "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",
+//   });
+
+// const msg = await client.messages.create({
+//   model: "claude-3-opus-20240229",
+//   max_tokens: 1000,
+//   temperature: 0,
+//   system: "Translate classical Armenian language to modern Armenian language\nUser will write classical Armenian language and we will give him translated text(modern Armenian language) in response",
+//   messages: [
+//     {
+//       "role": "user",
+//       "content": [
+//         {
+//           "type": "text",
+//           "text": "Իգական"
+//         }
+//       ]
+//     }
+//   ]
+// });
+// console.log(msg , "djkasjdkasdljasldka");

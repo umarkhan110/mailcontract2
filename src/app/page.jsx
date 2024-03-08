0c7457f5f41f21043e6180840256fb112491bb1b
umarkhan110 <umardraz277@gmail.com>
2024-03-08 18:52:13 +0500

    remove api key

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -38,31 +38,3 @@ const translate = async () =>{
     </>
   );
 }
-
-
-
-
-// import Anthropic from "@anthropic-ai/sdk";
-// // const client = new Anthropic();
-// const client = new Anthropic({
-//     apiKey: "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",
-//   });
-
-// const msg = await client.messages.create({
-//   model: "claude-3-opus-20240229",
-//   max_tokens: 1000,
-//   temperature: 0,
-//   system: "Translate classical Armenian language to modern Armenian language\nUser will write classical Armenian language and we will give him translated text(modern Armenian language) in response",
-//   messages: [
-//     {
-//       "role": "user",
-//       "content": [
-//         {
-//           "type": "text",
-//           "text": "Իգական"
-//         }
-//       ]
-//     }
-//   ]
-// });
-// console.log(msg , "djkasjdkasdljasldka");
